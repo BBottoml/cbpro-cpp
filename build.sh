e769b197664e10d1f46e68b869790a26d45e8291
BBottoml <bottomlee@purdue.edu>
2021-01-02 22:57:26 -0600

    Update dependency build script and CMakeLists.txt

--- build.sh
+++ build.sh
@@ -2,10 +2,18 @@
 
 mkdir temp
 cd temp
+
+# spdlog installation 
 git clone https://github.com/gabime/spdlog.git
 cd spdlog && mkdir build && cd build
-
 cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=../../../install .. && make -j 
 make install
-cd ../../../
+cd ../../
+
+# boost installation 
+wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.bz2
+tar --bzip2 -xf ./boost_1_66_0.tar.bz2
+mv ./boost_1_66_0.tat.bz2 ../install
+
+cd ../
 rm -rf temp
